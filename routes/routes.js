18a4a94517f0ae5c4e510e8b8a80be189b65a283
y3sh <joshhibschman@gmail.com>
2016-01-29 08:26:58 -0600

    last removals

--- routes/routes.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  HOST: "https://localhost:9001",
-  INSTRUMENT: "/instrument",
-  BEAUTIFY_JS: "/beautifyJS",
-  BEAUTIFY_HTML: "/beautifyHTML"
-};
--- util/flushRedis.js
+++
@@ -1,9 +0,0 @@
-
-var redis = require('redis');
-var redisClient = redis.createClient();
-
-redisClient.on('connect', function () {
-  redisClient.flushdb(function (err, didSucceed) {
-    console.log(didSucceed); // true
-  });
-});
